5cbe6c18878c6632163a48c703bdc2e9d96698d2
lukamijic <luka.mijic@five.agency>
2021-04-05 14:29:21 +0200

    general gradle and settings.gradel

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,15 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
+                useModule("com.android.tools.build:gradle:4.0.1")
+            }
+        }
+    }
 }
 rootProject.name = "Ludens"
 
-
 include(":androidApp")
 include(":shared")
-
